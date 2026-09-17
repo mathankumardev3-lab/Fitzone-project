fed5ad952ed15dbca34535ddf60d97695ab3a945
Your Name <your-email@example.com>
2026-09-17 13:58:56 +0530

    Fix media image serving

--- fitzone/urls.py
+++ fitzone/urls.py
@@ -1,12 +1,17 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("gym.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
